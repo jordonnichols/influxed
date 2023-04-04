c08b6eb1728eaee4178da4c3e4bd41c0d8bb19b5
influxion <nicholsjordon721@gmail.com>
2023-04-04 14:59:07 -0500

    fix

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -7,6 +7,9 @@ export default function HomePage() {
     <main className="max-w-[100dvw]">
       <HeroWrapper>
         <AsciiObject />
+        <h1 className="text-4xl font-bold absolute top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2 select-none text-center">
+          UNDER CONSTRUCTION
+        </h1>
       </HeroWrapper>
       <Footer />
     </main>
