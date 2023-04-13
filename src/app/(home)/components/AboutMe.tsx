2b02bf8e9efb82fb4bfae4339d4bd6c591ad6246
Jordon Nichols <nicholsjordon721@gmail.com>
2023-04-13 18:48:08 -0500

    vh

--- src/app/(home)/components/AboutMe.tsx
+++ src/app/(home)/components/AboutMe.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function AboutMe() {
   return (
-    <div className="h-screen w-full flex flex-col justify-center bg-neutral-900 z-20 relative">
+    <div className="h-[100dvh] w-full flex flex-col justify-center bg-neutral-900 z-20 relative">
       <div className="lg:py-48 py-24 max-w-[1232px] m-auto lg:overflow-visible overflow-hidden px-4">
         <div className="md:w-[500px] w-full flex flex-col gap-2">
           <p className="md:text-6xl text-2xl font-bold">
