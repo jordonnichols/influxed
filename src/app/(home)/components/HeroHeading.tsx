94be1fe8f74711a39046798f41e008edf09a69a1
Jordon Nichols <nicholsjordon721@gmail.com>
2023-04-14 17:33:39 -0500

    changes

--- src/app/(home)/components/HeroHeading.tsx
+++ src/app/(home)/components/HeroHeading.tsx
@@ -5,17 +5,19 @@ import JordonNichols from 'src/app/(GLOBALS)/components/JordonNichols'
 const HeroHeading = () => {
   return (
     <>
-      <div className="z-10 duration-100 select-none h-full w-full relative">
-        <div className="text-center lg:text-lef w-full">
-          <h1 className=" 3xl:text-14xl font-bold lg:leading-zero 2xl:text-13xl xl:text-12xl lg:text-11xl inline-block sm:text-center text-7xl leading-none">
-            influxed
-          </h1>
+      <div className="select-none w-full">
+        <div className="z-10 absolute top-0 right-1/2 translate-x-1/2 w-full">
+          <div className="text-center lg:text-lef w-full">
+            <h1 className=" 3xl:text-14xl font-bold lg:leading-zero 2xl:text-13xl xl:text-12xl lg:text-11xl inline-block sm:text-center text-7xl leading-none">
+              influxed
+            </h1>
+          </div>
+          <h2 className="lg:text-lef lg:text-3xl font-bold leading-zero text-center text-base 2xl:ml-5 2xl:text-5xl lg:ml-3">
+            <span className="font-light">developments, powered by </span>{' '}
+            <JordonNichols />
+          </h2>
         </div>
-        <h2 className="lg:text-lef lg:text-3xl font-bold leading-zero text-center text-base 2xl:ml-5 2xl:text-5xl lg:ml-3">
-          <span className="font-light">developments, powered by </span>{' '}
-          <JordonNichols />
-        </h2>
-        <div className="w-full absolute lg:bottom-12 bottom-4">
+        <div className="z-10 w-full absolute lg:bottom-12 bottom-4">
           <div className="md:w-[500px] w-full flex flex-col gap-2 m-auto px-4">
             <p className="md:text-6xl text-2xl font-bold">
               in·flux·ed{' '}
